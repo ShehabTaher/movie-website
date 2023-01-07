552c5b1ade18ffc7482e1b12f2c0878318da0566
Shehab Taher <shehab.taher93@gmail.com>
2023-01-08 01:53:58 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router ,Routes , Route} from 'react-router-dom';
-import  Header  from './components/header';
-import Watchlist from './components/watchlist';
-import Watched  from './components/watched';
-import Add from './components/add';
-import './App.css';
-import './lib/font-awesome/css/all.min.css'
-
-import { GlobalProvider } from './context/GlobalState';
+import {Route, Routes} from 'react-router-dom'
+import MovieList from './Movies/MovieList'
+import DesCard from './Description/DesCard'
+import "./App.css";
 
 function App() {
   return (
-    <GlobalProvider>
-    <Router>
-      <Header/>
-
-       <Routes>
-        <Route exact path='/' element={<Watchlist/>} />
-
-        <Route exact path='/watched' element={<Watched/>} />
-        <Route exact path='/add' element={<Add/>} />
-      </Routes> 
-    </Router>
-    </GlobalProvider>
-  )
+    <Routes>
+      <Route path="/" element={<MovieList/>}/>
+      <Route path=":id" element={<DesCard/>}/>
+    </Routes>
+  );
 }
 
 export default App;
